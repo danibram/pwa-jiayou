ba2e113cb70e288844431ca5dafd54495613a988
Daniel Biedma <info@dbr.io>
2022-08-08 14:49:47 +0200

    fix gtag

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,44 +1,42 @@
-import { GA_TRACKING_ID } from './env';
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-    window.gtag('config', GA_TRACKING_ID, {
-        page_path: url
-    });
+  window.gtag("config", process.env.NEXT_GA_TRACKING_ID, {
+    page_path: url,
+  });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({
-    action,
-    category,
-    label,
-    value
+  action,
+  category,
+  label,
+  value,
 }: {
-    action: string;
-    category: string;
-    label?: string;
-    value?: string;
+  action: string;
+  category: string;
+  label?: string;
+  value?: string;
 }) => {
-    let event = {
-        event_category: category
-    };
+  let event = {
+    event_category: category,
+  };
 
-    if (label) {
-        event['event_label'] = label;
-    }
+  if (label) {
+    event["event_label"] = label;
+  }
 
-    if (value) {
-        event['value'] = value;
-    }
+  if (value) {
+    event["value"] = value;
+  }
 
-    window.gtag('event', action, event);
+  window.gtag("event", action, event);
 };
 
 export const eventNi = ({ action, category, label, value }) => {
-    window.gtag('event', action, {
-        event_category: category,
-        event_label: label,
-        value: value,
-        non_interaction: true
-    });
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+    non_interaction: true,
+  });
 };
